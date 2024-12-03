361b7a33afb2c0188f4a7c2b6588f34875ed8e80
rychardaniel <164958686+rychardaniel@users.noreply.github.com>
2024-12-03 12:02:01 -0300

    Fim da aula 1
    Manipulando as cores e o ícone svg do cartão de crédito

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
 import "./css/index.css"
+
+const cor1Cartao = document.querySelector("#app > section > div.cc-bg > svg > g > g:nth-child(1) > path")
+
+const cor2Cartao = document.querySelector("#app > section > div.cc-bg > svg > g > g:nth-child(2) > path")
+
+const logoCartao = document.querySelector("#app > section > div.cc-logo > span:nth-child(2) > img")
+
+
+
+function setCardType(type) {
+    const colors = {
+        visa: ["#436D99", "#2D57F2"],
+        mastercard: ["#DF6F29", "#C69347"],
+        cielo: ["#5a646e", "#00aeef"],
+        default: ["black", "gray"]
+    }
+
+    const logos = {
+        visa: "public/cc-visa.svg",
+        mastercard: "public/cc-mastercard.svg",
+        cielo: "public/cc-cielo.svg",
+        default: "public/cc-default.svg"
+    }
+
+    cor1Cartao.setAttribute("fill", colors[type][0])
+    cor2Cartao.setAttribute("fill", colors[type][1])
+    logoCartao.setAttribute("src", logos[type])
+}
+
+setCardType("default")
+
+globalThis.setCardType = setCardType
