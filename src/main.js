c60d4b25f78a0597d65885d31f53d73741dfd127
rychardaniel <164958686+rychardaniel@users.noreply.github.com>
2024-12-06 00:26:06 -0300

    Formatação melhor e adição de comentários para lembrar futuramente

--- src/main.js
+++ src/main.js
@@ -2,13 +2,10 @@ import "./css/index.css"
 import IMask from "imask"
 
 const cor1Cartao = document.querySelector("#app > section > div.cc-bg > svg > g > g:nth-child(1) > path")
-
 const cor2Cartao = document.querySelector("#app > section > div.cc-bg > svg > g > g:nth-child(2) > path")
-
 const logoCartao = document.querySelector("#app > section > div.cc-logo > span:nth-child(2) > img")
 
-
-
+// Configurar as cores do cartão e a imagem da bandeira
 function setCardType(type) {
     const colors = {
         visa: ["#436D99", "#2D57F2"],
@@ -29,33 +26,34 @@ function setCardType(type) {
     logoCartao.setAttribute("src", logos[type])
 }
 
-const securityCode = document.querySelector("#security-code")
+// Criação de máscaras para os inputs de dados
+const securityCode = document.querySelector("#security-code") // Pega o input de CVC
 
-const securityCodePattern = {
+const securityCodePattern = { // Define uma máscara que aceita somente 4 ou menos números
     mask: "0000"
 }
 
-const securityCodeMasked = IMask(securityCode, securityCodePattern)
+const securityCodeMasked = IMask(securityCode, securityCodePattern) // Aplica a máscara no input e define uma variável que contem o valor do input com a máscara aplicada
 
-const expirationDate = document.querySelector("#expiration-date")
+const expirationDate = document.querySelector("#expiration-date") // Pega o input da data de expiração do cartão
 
-const expirationDatePattern = {
+const expirationDatePattern = { // Define uma máscara para o formato MM/YY sendo o MM o mês e o YY o ano
     mask: 'MM{/}YY',
     blocks: {
         MM: {
-            mask: IMask.MaskedRange,
-            from: 1,
-            to: 12
+            mask: IMask.MaskedRange, // Deve ser essa propriedade do IMask para definir um intervalor de valor
+            from: 1, // Início do intervalo (incluso)
+            to: 12 // Fim do intervalo (incluso)
         },
         YY: {
             mask: IMask.MaskedRange,
-            from: String(new Date().getFullYear()).substring(2),
-            to: String(new Date().getFullYear() + 10).substring(2)
+            from: String(new Date().getFullYear()).substring(2), // Pega o ano atual, transforma em string ("2024"), e seleciona a partir do 3º dígito ("24")
+            to: String(new Date().getFullYear() + 10).substring(2) // // Pega o ano atual, soma 10, transforma em string ("2034"), e seleciona a partir do 3º dígito ("34")
         }
     }
 }
 
-const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern) // Aplica a máscara no input (input, máscara), e adiciona a uma váriavel o valor do input com máscara
 
 setCardType("default")
 
